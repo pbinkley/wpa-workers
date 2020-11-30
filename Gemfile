d4d01599c2b29067c54a6baccdd74187012cf615
Peter Binkley <peter.binkley@ualberta.ca>
2020-11-30 11:32:05 -0700

    WIP

--- Gemfile
+++ Gemfile
@@ -7,3 +7,7 @@ gemspec
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'kramdown-parser-gfm'
 gem 'byebug'
+
+#gem 'wax_tasks', github: 'pbinkley/wax_tasks', branch: 'annotationlists'
+gem 'wax_tasks', path: '/home/pbinkley/Projects/wax/recipebook/wax_tasks'
+gem 'wax_iiif', path: '/home/pbinkley/Projects/wax/wax_iiif-pbinkley-fork'
--- _workshop/generate-signature-annotations.rb
+++ _workshop/generate-signature-annotations.rb
@@ -3,6 +3,7 @@
 
 require 'json'
 require 'csv'
+require 'yaml'
 require 'byebug'
 
 list_dimensions = [
@@ -11,7 +12,7 @@ list_dimensions = [
 ]
 
 width = 505 - 127
-overlap = 15
+overlap = 0
 
 item_signatures = {}
 page_num = 1
@@ -38,7 +39,27 @@ list_dimensions.each do |page|
       signatures[col2_id] = [worker['signature'], col2_x, y, width, height + overlap] 
     end
   end
-  item_signatures[page_num] = signatures
+  item_signatures[page_num] = signatures.sort_by { |e| e.first }
+
+  resources = []
+  item_signatures[page_num].each do |signature|
+    worker = workers.find {|worker| worker['id'] == signature.first.to_s}
+    resources << {
+      'xywh' => signature[1][1..4].join(','),
+      'chars' => "<h4>#{signature.first}: #{signature[1][0]}</h4><p><em>Census Data</em>: occupation: '#{worker['occupation_28']}'; industry: '#{worker['industry_29']}'</p>"
+    }
+  end
+
+  signature_yaml = {
+    'uri' => "{{ '/' | absolute_url }}img/derivatives/iiif/annotation/doc9031_#{page_num}.json",
+    'collection' => 'documents',
+    'canvas' => page_num.to_s,
+    'label' => "doc9031_#{page_num}",
+    'target' => "{{ '/' | absolute_url }}img/derivatives/iiif/canvas/doc9031_#{page_num}.json",
+    'resources' => resources
+  }
+
+  File.open("./doc9031_#{page_num}.yaml", 'w') { |file| file.write(signature_yaml.to_yaml) }
 
   page_num += 1
   row_offset += (page[:rows] * 2)

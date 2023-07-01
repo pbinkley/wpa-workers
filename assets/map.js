40d28f17850ff29b39b79ce168048dfd52b61f01
Peter Binkley <pbinkley@users.noreply.github.com>
2023-07-01 13:36:30 -0600

    Update Mapbox access token

--- assets/map.js
+++ assets/map.js
@@ -4,7 +4,7 @@ permalink: /assets/map.js
 
   var map = L.map('mapid');
 
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicGJpbmtsZXkiLCJhIjoiY2xqa2VnMzJxMGhhazNmbDV2eDMzbTVtaiJ9.TOAKD8tLddNL8ETVB9lteg', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
